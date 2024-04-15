d67d92cf490db29b66e6a32951c6559539713410
rizwan <rizwan@aptechsite.net>
2024-04-15 18:06:26 +0500

    Update DumTest.js

--- react01-app/src/DumTest.js
+++ react01-app/src/DumTest.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 function Heading(){
-    return <h1>Pakistan</h1>
+    return <> <h1 style={{background:'red',color:'green'}}>Pakistan</h1><h2>Lorem.......</h2></>
 }
 function Heading1(){
     return <h1>Zindabad</h1>
