a2f4eca18972d1ffa5231c8b160b720946998191
rizwan <rizwan@aptechsite.net>
2024-05-10 18:48:01 +0500

    Update App.js

--- react06-app/src/App.js
+++ react06-app/src/App.js
@@ -5,25 +5,20 @@ import { useRef,useState } from 'react';
 function App() {
   let userref = useRef();
   let [usemsg,setusermsg] = useState('');
+  let [IsValidEmail,SetEmailValidation] =useState('');
   const OnButtonClick = ()=>{
     const reg = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
     console.log(userref.current.value)
-    const isvalid = reg.test(userref.current.value);
-    console.log(isvalid)
-    
-    if(userref.current.value == ""){
-
-      setusermsg("Input box is Empty")
-    }
-    else{
-
-
-    }
+    const isvalid = reg.test(userref.current.value)? true:false ;
+    SetEmailValidation(isvalid);
+   
+   
   }
+  
   return (
     <div className="App">
     <input  type='text' ref={userref} /> 
-    <p>{usemsg}</p>
+    <p>{IsValidEmail}</p>
     <button onClick={OnButtonClick}>Click</button> 
     </div>
   );
